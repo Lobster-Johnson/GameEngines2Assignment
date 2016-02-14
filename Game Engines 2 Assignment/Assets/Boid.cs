55ec3e09a4a6f7fb70be837c1a70dc0235f2133f
Marcus Daly <daly.marcus@gmail.com>
2016-02-14 17:54:23 +0000

    Script and Basic Scene

---
+++ Game Engines 2 Assignment/Assets/Boid.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour {
+    public Vector3 velocity;
+    public Vector3 acceleration;
+    public Vector3 force;
+    public float mass;
+
+    public float maxSpeed = 5.0f;
+    public float maxForce = 5.0f;
+
+    public bool seekEnabled;
+    public Vector3 seekTargetPosition;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    Vector3 Seek(Vector3 target)
+    {
+        Vector3 toTarget = target - transform.position;
+        toTarget.Normalize();
+        Vector3 desired = toTarget * maxSpeed;
+        return desired - velocity;
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+        force = Vector3.zero;
+
+        if (seekEnabled)
+        {
+            force = Seek(seekTargetPosition);
+        }
+
+        force = Vector3.ClampMagnitude(force, maxForce);
+        acceleration = force / mass;
+        velocity += acceleration * Time.deltaTime;
+
+        velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+        transform.position += velocity * Time.deltaTime;
+        if (velocity.magnitude > float.Epsilon)
+        {
+            transform.forward = velocity;
+        }
+    }
+}
---
+++ Game Engines 2 Assignment/Assets/Fighterspawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Fighterspawner : MonoBehaviour
+{
+    public GameObject fighter;
+    public int squadSize = 3;
+    public int x = 1;
+    public int y = 6;
+    public int z = -3;
+    // Use this for initialization
+    void Start ()
+    {
+        
+        //spawn in the ships and give them orders
+        for(int i = 0; i < squadSize; i++)
+        {
+            //spawn ships in a line
+            GameObject Ship= Instantiate(fighter);
+            Vector3 start = new Vector3(x, y, z);
+            Ship.transform.position = start;
+            x = x + 3;
+
+            //give orders
+            Ship.GetComponent<Navigation>();
+
+            Vector3 p1 = new Vector3(x, 3, -2);
+            Vector3 p2 = new Vector3(x, 0, -1);
+            Vector3 p3 = new Vector3(x, -3, 0);
+            Ship.GetComponent<Navigation>().neworders(p1);
+            Ship.GetComponent<Navigation>().neworders(p2);
+            Ship.GetComponent<Navigation>().neworders(p3);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
